8c44782278b3a481d60bf2b327e4bc68542a1392
Nihar Patil <nihar.patil98@gmail.com>
2017-03-04 00:34:35 -0500

    fixed port

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ var server = express();
 
 
 server.use(express.static('public'));
-var port = 3000;
 
+var port = process.env.PORT || 3000;
 
 server.listen(port, function(){
 	console.log('listening on port ' + port);
